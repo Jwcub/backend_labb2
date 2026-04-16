e8644c26bcbe4c09642626707bbf465cb8b3fe73
Kevin.M <kema2501@student.miun.se>
2026-04-16 14:08:15 +0200

    Lagt till funktionalitet för att lägga till nya tjänster

--- server.js
+++ server.js
@@ -17,17 +17,55 @@ const client = createClient({
 app.use(cors()); // Tillåter cross-origin
 app.use(express.json()); //Parsa JSON-body
 
+// Routing - lista alla jobb
+app.get("/jobs", async(req, res) => {
+    try {
+        const jobs = await client.execute("SELECT * FROM workexperience");
+        res.json(jobs.rows)
+    } catch (e) {
+        console.error("Något gick fel:", e);
+         res.status(500).send("Internt serverfel");
+    }
+});
 
-async function run() {
-  try {
-    const rs = await client.execute("SELECT * FROM workexperience");
-    console.log(rs.rows);
-  } catch (e) {
-    console.error("Något gick fel:", e);
-  }
-}
+// Routing - lista jobb med id
+app.get("/jobs/:id", async(req, res) => {
+    try {
+        const jobId = req.params.id;
 
-run();
+        const job = await client.execute({
+            sql: "SELECT * FROM workexperience WHERE id = :id",
+            args: { id: jobId }
+        });
+        if(!job) return res.status(404).json({ error: "Inget jobb hittades" });
+        res.json(job.rows);
+
+    } catch (e) {
+        console.error("Något gick fel:", e);
+         res.status(500).send("Internt serverfel");
+    }
+});
+
+// Routing - lägga till nya jobb
+app.post("/jobs", async(req, res) => {
+    const { companyname, jobtitle, joblocation, startdate, enddate, description } = req.body;
+
+    if(!companyname || !jobtitle || !joblocation || !startdate) {
+        return res.status(400).json({ messsage: "Företagsnamnn, jobbtitel, plats samt  anställnings startdatum krävs" })
+    }
+
+    try {
+        const results = await client.execute({
+            sql: "INSERT INTO workexperience(companyname, jobtitle, joblocation, startdate, enddate, description) VALUES (?, ?, ?, ?, ?, ?)",
+            args: [companyname, jobtitle, joblocation, startdate, enddate || null, description || null]
+        });
+        res.status(201).json({ id: results.lastInsertRowid?.toString(), ...req.body });
+    } catch(error) {
+        console.error("Åtgärd misslyckaded:", error);
+        res.status(500).json({ message: "Kunde inte lägga till tjänst" });
+    }
+});
+
 
 //Starta applikation
 app.listen(3000, () => {
